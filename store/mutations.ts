05dafebd61e8805ba56e219927005b3a2da03f46
Joshua Grierson <encredgaming@gmail.com>
2019-01-28 12:11:30 +0000

    [mutations] - added comment to set timeout function

--- store/mutations.ts
+++ store/mutations.ts
@@ -2,6 +2,7 @@ import { ApiData } from './types/api';
 import { RootState } from './types/state';
 
 export const renderWeatherData = (state: RootState, data: ApiData) => {
+  // delay purposely for smoother transition between placeholder and data
   setTimeout(() => {
     state.loading = false;
     state.data = data.data;
